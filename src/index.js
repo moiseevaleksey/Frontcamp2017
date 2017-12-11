247a05fc75b1bb06c7f05776ceb4eb7166ce2f95
Aliaksei Maiseyeu <Aliaksei_Maiseyeu@epam.com>
2017-12-11 12:59:24 +0300

    custom loader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import Application from './classes/application'
+import Application from './classes/application';
 import './style.css';
+import a from './test';
 
 let app = new Application();
+
+let q = JSON.parse(a);
 
 app.loadSources();
--- webpack.config.js
+++ webpack.config.js
@@ -10,17 +10,41 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    resolveLoader: {
+        modules: ['node_modules', 'loader'],
+    },
     module: {
-        loaders: [
+        rules: [{
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: /node_modules/,
+        }, {
+            test: /\.css$/,
+            loader: 'style-loader!css-loader',
+        },
             {
-                test: /\.js$/,
-                use: 'babel-loader',
-                exclude: /node_modules/,
-            }, {
-                test: /\.css$/,
-                loader: 'style-loader!css-loader',
+                test: /\.(json)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(json)$/,
+                use: [
+                    {
+                        loader: 'my-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
+                ]
             }
-        ],
+        ]
     },
     resolve: {
         extensions: ['.js', '.json'],
