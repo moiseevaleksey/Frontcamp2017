471e1046c0ee3b4b93362373c73bff49c923575f
moiseevaleksey <moiseevaleksey92@gmail.com>
2018-01-02 22:51:45 +0300

    5

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import LoadNewsButton from './classes/button';
 import './style.css';
 import incRating from './classes/decorator';
 import Article from './classes/article';
+import createStore from './classes/redux';
 
 const showNewsButton = new LoadNewsButton('Show News');
 
@@ -20,3 +21,19 @@ showNewsButton.init();
 
 // console.log(art.raiting());
 
+
+const store = createStore(reducer);
+
+const foo = () => {
+    console.log('DISPATCHED!');
+};
+
+store.subscribe(foo);
+
+store.dispatch({
+    type: 'ADD_COMPANY',
+    payload: 'epam'
+});
+
+console.log('current sate : ', store.getState());
+store.unsubscribe(foo);
